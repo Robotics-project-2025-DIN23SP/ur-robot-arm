9bae7f601e95b845d489ccc3dd08386659330c0b
Helmi Griffiths <t3grhe00@students.aomk.fi>
2025-11-18 17:18:44 +0200

    chore: remove unnecessary comments after merge

--- ur_arm/ur_arm_config.py
+++ ur_arm/ur_arm_config.py
@@ -21,13 +21,11 @@ POSITION_APPROACH_ITEM_1: List[float] = [1.089783, -0.36913, 0.83095, -2.047795,
 POSITION_GRASP_ITEM_1: List[float] = [1.089783, -0.34, 0.83095, -2.047795, -1.578127, -0.563741]
 
 # positions for item_2
-POSITION_APPROACH_ITEM_2: List[float] = [1.23255, -1.22732, 1.68145, -2.02458, -1.57080, -0.33859]  # base, shoulder, elbow, wrist1, wrist2, wrist3
-POSITION_GRASP_ITEM_2: List[float] = [1.23255, -0.95086, 1.91044, -2.53003, -1.57080, -0.33912]  # base, shoulder, elbow, wrist1, wrist2, wrist3
+POSITION_APPROACH_ITEM_2: List[float] = [1.23255, -1.22732, 1.68145, -2.02458, -1.57080, -0.33859]
+POSITION_GRASP_ITEM_2: List[float] = [1.23255, -0.95086, 1.91044, -2.53003, -1.57080, -0.33912]
 
 # positions for item_3 
 POSITION_APPROACH_ITEM_3: List[float] = [1.2547, -0.5639, 1.04231, -2.04779, -1.56940, -0.316428]
 POSITION_GRASP_ITEM_3: List[float] = [1.2547, -0.43737, 1.06901, -2.201209, -1.56940, -0.316428]
 
 # TODO: positions for item_4
-#POSITION_APPROACH_ITEM_4: List[float] = []#base, shoulder, elbow, wrist1, wrist2, wrist3
-#POSITION_GRASP_ITEM_4: List[float] = []#base, shoulder, elbow, wrist1, wrist2, wrist3
